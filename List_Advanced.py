fb7998d7277f877aa3d8a564c4cd6754e7a2fab3
u2cc <cheng.james.chen@gmail.com>
2022-01-09 14:44:58 -0500

    Notes of Practice 9th Jan 2022

--- List_Advanced.py
+++ List_Advanced.py
@@ -26,3 +26,25 @@ print(min(digits))
 print(max(digits))
 print(sum(digits))
 
+#list comprehension
+squares = [value**2 for value in range(1,11)]
+print(squares)
+
+#slicing a list
+brands = ['NIKE','DELL','Apple','Microsoft']
+print(brands[0:3])
+print(brands[:3])
+print(brands[1:])
+print(brands[-2:]) #prints last two using negative numerics
+
+for brand in brands[-2:]:
+    print(brand)
+
+my_cars = ['Lexus','Toyota']
+my_friend_cars = my_cars[:]
+
+my_cars.append('Ford')
+my_friend_cars.append('Ram')
+
+print(f'My cars: {my_cars}')
+print(f'\nMy friend\'s cars: {my_friend_cars}')
---
+++ Tuple.py
@@ -0,0 +1,8 @@
+dimensions = (200, 50, 30)
+print(dimensions[0])
+print(dimensions[1])
+
+#single value tuple, the comma is mandatory and the parentheses are cosmetical
+constant = 1,
+print(constant)
+#constant[0] = 2
